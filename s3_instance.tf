ae5dbf1f8427a3de3f4134e374b6482b0303eaac
muhammad usman haider <musmanvk18@gmail.com>
2024-10-16 21:44:21 +0500

    set s3 bucket state

--- s3_instance.tf
+++ s3_instance.tf
@@ -1,10 +1,8 @@
-resource "aws_s3_object" "object" {
+# Create the S3 bucket
+resource "aws_s3_bucket" "terraform_bucket" {
   bucket = "terraform-state-usman"
-  key    = ""
-  source = "path/to/file"
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("path/to/file")
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Production"
+  }
 }
